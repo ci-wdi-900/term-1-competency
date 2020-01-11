44b017bd5d1c0093faf89bfc91b6e3642794bbad
Colin Jaffe <balloonasaurus@gmail.com>
2020-01-11 18:00:25 -0500

    Initial commit.

---
+++ main.js
@@ -0,0 +1,34 @@
+function fizzBuzzSingleNumber() {
+
+}
+
+function rotateLetters() {
+
+}
+
+function uppercaseCount() {
+
+}
+
+function addToMultiDigitNumbers() {
+
+}
+
+
+function singlesOnly() {
+
+}
+
+function getMeanAverageSalaryOfWomen() {
+
+}
+
+
+module.exports = {
+  fizzBuzzSingleNumber,
+  rotateLetters,
+  uppercaseCount,
+  addToMultiDigitNumbers,
+  singlesOnly,
+  getMeanAverageSalaryOfWomen,
+}
